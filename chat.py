06b46671b34c0307a3d8202e1fbb05c62f8a501a
cervone23 <51930581+cervone23@users.noreply.github.com>
2023-01-19 09:22:32 -0500

    Update chat.py

--- chat.py
+++ chat.py
@@ -2,6 +2,42 @@ import streamlit as st
 from streamlit_chat import message
 import openai
 from PIL import Image
+import base64
+
+# CSS 
+
+@st.experimental_memo
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+img = get_img_as_base64("image.jpg")
+
+page_bg_image = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
+background-size: 180%;
+background-position: top left;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+[data-testid="stSidebar"] > div:first-child {{
+background-image: url("data:image/png;base64,{img}");
+background-position: center; 
+background-repeat: no-repeat;
+background-attachment: fixed;
+}}
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
+</style>
+"""
+st.markdown(page_bg_image, unsafe_allow_html = True)
 
 openai.api_key = st.secrets['api_key']
 
@@ -55,5 +91,4 @@ if st.session_state['generated']:
     for i in range(len(st.session_state['generated'])-1, -1, -1):
         message(st.session_state["generated"][i], seed=86, key=str(i))
         message(st.session_state['past'][i], is_user=True, 
-                avatar_style='fun-emoji', seed='Aneka', key=str(i) + '_user')
-
+                avatar_style='fun-emoji', key=str(i) + '_user')
